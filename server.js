d6453826604703581fd074b66821123d83bc1cc5
jeniwitness <janesianga2002@gmail.com>
2026-02-12 02:37:25 -0800

    i made some changes to the server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express =require('express');
 const app=express();
 app.get('/',(req,res)=>{
-    res.send('Hellow from Node.js ! and am Janewitness J Siangs');
+    res.send('Hellow from Node.js ! and am Janewitness J Sianga and I LOVE YOU ALL');
 });
 app.listen(3000,()=>{
     console.log('Server running on port 3000');
